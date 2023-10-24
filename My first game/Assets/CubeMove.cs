e337321ca9fbdba2e27091cf78e4f45f89057d12
STUDENT Donnacha ODonoghue <Donnacha.ODonoghue@students.ittralee.ie>
2023-10-24 14:33:44 +0100

    Updated character and camera Signed-off-by: STUDENT Donnacha ODonoghue <Donnacha.ODonoghue@students.ittralee.ie>

--- My first game/Assets/CubeMove.cs
+++ My first game/Assets/CubeMove.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Donnacha : MonoBehaviour
+public class CubeMove : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()

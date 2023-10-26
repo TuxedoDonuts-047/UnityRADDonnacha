de6c7276abf1805b4730dcd3b4bf53c7448c3851
STUDENT Donnacha ODonoghue <Donnacha.ODonoghue@students.ittralee.ie>
2023-10-26 10:58:08 +0100

    Inserted ball model and plane Signed-off-by: STUDENT Donnacha ODonoghue <Donnacha.ODonoghue@students.ittralee.ie>

---
+++ My first game/Assets/BallControlScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControlScript : MonoBehaviour
+{
+    private Rigidbody rb;
+    float kickStrength = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.AddForce(kickStrength * Vector3.up, ForceMode.Impulse);
+        }
+
+    }
+}

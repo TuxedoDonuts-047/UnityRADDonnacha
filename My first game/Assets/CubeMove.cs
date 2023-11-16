08115690ce98f943b672a38794b686bfe808e10f
Donnacha O Donoghue <tucca003@gmail.com>
2023-11-16 00:15:35 +0000

    Added extra shapes Signed-off-by: Donnacha O Donoghue <tucca003@gmail.com>

--- My first game/Assets/CubeMove.cs
+++ My first game/Assets/CubeMove.cs
@@ -4,20 +4,19 @@ using UnityEngine;
 
 public class CubeMove : MonoBehaviour
 {
-    Rigidbody rb;
     public Transform footballCloneTemplate;
     float runSpeed=15;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.W))
-            rb.AddForce(transform.forward);
+            transform.position += runSpeed * transform.forward * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.S))
             transform.position -= runSpeed * transform.forward * Time.deltaTime;
